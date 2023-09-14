168db4b40f1df17a93f37fc13cf58d46f89a0af8
ivanmonahov75 <ivanmonahov75@gmail.com>
2023-09-14 11:55:15 -0700

    fixed: time is working

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import telebot
 from sentence_determinant import SentenceDeterminant
 import datetime
-
+from faq import current_time
 # our bot's token:
 token = '6054282792:AAG3MKRoB_31Nn39kMBQHhIGHbaEdGo_FJg'
 
@@ -51,7 +51,11 @@ def ret_sum(message):
 # I don't understand it completely, but bot will reply to anything with same text
 @bot.message_handler(func=lambda message: True)
 def irritate(message):
-    bot.reply_to(message, sen_det.get_closest_question(message.text))
+    if (sen_det.get_closest_question(message.text) is current_time):
+        func = sen_det.get_closest_question(message.text)
+        bot.reply_to(message, func())
+    else:
+        bot.reply_to(message, sen_det.get_closest_question(message.text))
 
 bot.infinity_polling()
 
