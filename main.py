3134854bc9992bb40af9f9f7ae0c246626175dc6
ivanmonahov75 <ivanmonahov75@gmail.com>
2023-09-19 21:29:12 +0300

    hotfix

--- main.py
+++ main.py
@@ -5,10 +5,23 @@ import datetime
 from faq import current_time
 
 
-def log():
-    logg = open('all.log', 'a')
-    logg.write('\n')
+def log(_type, mess):  # basal log function
+    logg = open('logs/all.log', 'a')
+    logg.write(f'{datetime.datetime.now()}, {_type}, {mess}\n')
     logg.close()
+    return f'{datetime.datetime.now()}, {_type}, {mess}\n'
+
+
+def log_new(username, chat_id):
+    return log('new user', f'username: {username}, chatID: {chat_id}')
+
+
+def log_mess(username, chat_id, mess, reply):
+    return log('message', f'username: {username}, chatID: {chat_id}, message: {mess}, reply: {reply}')
+
+
+def log_command(username, chat_id, mess, reply):
+    return log('command', f'username: {username}, chatID: {chat_id}, message: {mess}, reply: {reply}')
 
 
 # our bots token:
@@ -22,11 +35,8 @@ sen_det = SentenceDeterminant()
 # here is example, where bot will reply to comand 'start' to the chat, from where he was texted fraze
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    log = open('logs/all.log', 'a')
-
     bot.send_message(message.chat.id, 'hello there')
-    log.write(f'new user, chat id = {message.chat.id}\n')
-    log.close()
+    print(log_new(message.from_user.username, message.chat.id))
 
 
 @bot.message_handler(commands=['time'])
@@ -37,11 +47,13 @@ def ret_sum(message):
 # I don't understand it completely, but bot will reply to anything with same text
 @bot.message_handler(func=lambda message: True)
 def irritate(message):
-    if (sen_det.get_closest_question(message.text) is current_time):
-        func = sen_det.get_closest_question(message.text)
+    reply = sen_det.get_closest_question(message.text)
+    if reply is current_time:
+        func = reply
         bot.reply_to(message, func())
     else:
-        bot.reply_to(message, sen_det.get_closest_question(message.text))
+        bot.reply_to(message, reply)
+    log_mess(message.from_user.username, message.chat.id, message.text, reply)
 
 
 bot.infinity_polling()
